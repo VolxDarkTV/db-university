e7c85fbf19b1574bb650128d455b065fd1852275
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-01-20 15:38:26 +0100

    Group By

--- db_university.sql
+++ db_university.sql
@@ -1,3 +1,5 @@
+-- DAY 2023-01-19
+
 -- 1. Selezionare tutti gli studenti nati nel 1990 
 SELECT * FROM students 
 WHERE date_of_birth BETWEEN 1990-01-01 
@@ -31,3 +33,21 @@ SELECT COUNT(id) FROM departments;
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 SELECT * FROM teachers 
 WHERE phone IS null;
+
+-- DAY 2023-01-20
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT YEAR(enrolment_date) AS "Anno", COUNT(id) AS "iscrizioni_per_anno" FROM students
+GROUP BY YEAR(enrolment_date);
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT teachers.office_address, COUNT(office_address) AS "numero_uffici_presidiati" FROM teachers
+GROUP BY office_address;
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT exam_id, AVG(vote) AS "media_voto" FROM exam_student
+GROUP BY exam_id;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT department_id,COUNT(department_id), name FROM degrees
+GROUP BY department_id, name;
