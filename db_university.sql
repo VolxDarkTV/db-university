ec0f141ce7215f7d4ee68f23022f31ecf7e3d6fd
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-01-20 16:31:18 +0100

    add 2 join

--- db_university.sql
+++ db_university.sql
@@ -49,5 +49,25 @@ SELECT exam_id, AVG(vote) AS "media_voto" FROM exam_student
 GROUP BY exam_id;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
-SELECT department_id,COUNT(department_id), name FROM degrees
-GROUP BY department_id, name;
+SELECT department_id,COUNT(department_id) FROM degrees
+GROUP BY department_id;
+
+-- Join
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT * FROM students
+    JOIN degrees
+        ON students.id = degrees.department_id 
+WHERE degrees.name LIKE "Corso di Laurea in Economia";
+
+-- or More
+SELECT students.id, students.degree_id, students.name, students.surname, students.fiscal_code, students.registration_number, degrees.id, degrees.name FROM students
+    JOIN degrees
+        ON students.id = degrees.department_id 
+WHERE degrees.name LIKE "Corso di Laurea in Economia";
+
+-- 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+SELECT * FROM departments
+    JOIN degrees
+        ON departments.id = degrees.department_id
+WHERE departments.name LIKE "Dipartimento di Neuroscienze" AND degrees.level LIKE "magistrale";
+
